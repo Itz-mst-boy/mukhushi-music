bd814122b1deef9bfa7eba8fed59b853f2ac63b0
ᴍᴜᴋᴇsʜ <100356452+Itz-mst-boy@users.noreply.github.com>
2022-05-15 20:19:56 +0530

    Add files via upload

---
+++ HeroMusic/plugins/sudo/logger.py
@@ -0,0 +1,28 @@
+from pyrogram import filters
+import config
+from strings import get_command
+from HeroMusic import app
+from HeroMusic.misc import SUDOERS
+from HeroMusic.utils.database import add_off, add_on
+from HeroMusic.utils.decorators.language import language
+
+# Commands
+LOGGER_COMMAND = get_command("LOGGER_COMMAND")
+
+
+@app.on_message(filters.command(LOGGER_COMMAND) & SUDOERS)
+@language
+async def logger(client, message, _):
+    usage = _["log_1"]
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    state = message.text.split(None, 1)[1].strip()
+    state = state.lower()
+    if state == "enable":
+        await add_on(config.LOG)
+        await message.reply_text(_["log_2"])
+    elif state == "disable":
+        await add_off(config.LOG)
+        await message.reply_text(_["log_3"])
+    else:
+        await message.reply_text(usage)

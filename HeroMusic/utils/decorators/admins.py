b8960da3baf85f40c5f518dcc4ae0ce62c173724
ᴍᴜᴋᴇsʜ <100356452+Itz-mst-boy@users.noreply.github.com>
2022-05-16 12:41:47 +0530

    Update admins.py

--- HeroMusic/utils/decorators/admins.py
+++ HeroMusic/utils/decorators/admins.py
@@ -17,7 +17,7 @@ def AdminRightsCheck(mystic):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    "Bot is under maintenance. Please wait for some time..."
+                    "ʙᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ.ᴘʟᴇᴀsᴇ  ᴡᴀɪᴛ ғᴏʀ sᴏᴍᴇ ᴛɪᴍᴇ..."
                 )
         if await is_commanddelete_on(message.chat.id):
             try:
@@ -34,7 +34,7 @@ def AdminRightsCheck(mystic):
                 [
                     [
                         InlineKeyboardButton(
-                            text="How to Fix this? ",
+                            text="ʜᴏᴡ ᴛᴏ ғɪx ᴛʜɪs? ",
                             callback_data="AnonymousAdmin",
                         ),
                     ]
@@ -74,7 +74,7 @@ def AdminActual(mystic):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    "Bot is under maintenance. Please wait for some time..."
+                    "ʙᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ.ᴘʟᴇᴀsᴇ  ᴡᴀɪᴛ ғᴏʀ sᴏᴍᴇ ᴛɪᴍᴇ..."
                 )
         if await is_commanddelete_on(message.chat.id):
             try:
@@ -91,7 +91,7 @@ def AdminActual(mystic):
                 [
                     [
                         InlineKeyboardButton(
-                            text="How to Fix this? ",
+                            text="ʜᴏᴡ ᴛᴏ ғɪx ᴛʜɪs? ",
                             callback_data="AnonymousAdmin",
                         ),
                     ]
@@ -119,7 +119,7 @@ def ActualAdminCB(mystic):
         if await is_maintenance() is False:
             if CallbackQuery.from_user.id not in SUDOERS:
                 return await CallbackQuery.answer(
-                    "Bot is under maintenance. Please wait for some time...",
+                    "ʙᴏᴛ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ.ᴘʟᴇᴀsᴇ  ᴡᴀɪᴛ ғᴏʀ sᴏᴍᴇ ᴛɪᴍᴇ...",
                     show_alert=True,
                 )
         try:

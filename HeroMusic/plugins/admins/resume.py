55a2a01d3e572b24e9bf2a47c85cf73e436a8930
ᴍᴜᴋᴇsʜ <100356452+Itz-mst-boy@users.noreply.github.com>
2022-05-16 21:41:40 +0530

    Add files via upload

--- HeroMusic/plugins/admins/resume.py
+++ HeroMusic/plugins/admins/resume.py
@@ -1,28 +1,30 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from HeroMusic.utils.database import set_loop
 from config import BANNED_USERS
 from strings import get_command
 from HeroMusic import app
 from HeroMusic.core.call import Hero
+from HeroMusic.utils.database import is_music_playing, music_on
 from HeroMusic.utils.decorators import AdminRightsCheck
 
 # Commands
-STOP_COMMAND = get_command("STOP_COMMAND")
+RESUME_COMMAND = get_command("RESUME_COMMAND")
 
 
 @app.on_message(
-    filters.command(STOP_COMMAND)
+    filters.command(RESUME_COMMAND)
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
 )
 @AdminRightsCheck
-async def stop_music(cli, message: Message, _, chat_id):
+async def resume_com(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    await Hero.stop_stream(chat_id)
-    await set_loop(chat_id, 0)
+    if await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_3"])
+    await music_on(chat_id)
+    await Hero.resume_stream(chat_id)
     await message.reply_text(
-        _["admin_9"].format(message.from_user.mention)
+        _["admin_4"].format(message.from_user.mention)
     )

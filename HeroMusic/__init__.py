a6f7a52fb69143b6eb6af48ac2a974f3e9b54461
ᴍᴜᴋᴇsʜ <100356452+Itz-mst-boy@users.noreply.github.com>
2022-05-16 21:50:59 +0530

    Add files via upload

---
+++ HeroMusic/__init__.py
@@ -0,0 +1,38 @@
+from HeroMusic.core.bot import HeroBot
+from HeroMusic.core.dir import dirr
+from HeroMusic.core.git import git
+from HeroMusic.core.userbot import Userbot
+from HeroMusic.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = HeroBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
